1ef4a66fb895b55fedcc2fdddc907bc2a366d0f5
Vinay <vinnupatil007@gmail.com>
2024-10-15 15:06:45 +0530

    add inputs

---
+++ src/Sept2024/Lab004.java
@@ -0,0 +1,10 @@
+package Sept2024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+
+        int Vinay = 100;
+        int Harish = 200;
+        String result = Vinay > Harish ? "Vinay is great" : "Harish is great";
+    }
+}
